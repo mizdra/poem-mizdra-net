b550b49ec81f753cc47e1c0a7d6afdfc707ddd88
mizdra <pp.mizdra@gmail.com>
2023-01-18 00:07:23 +0900

    Node にする

--- backend/graphql/index.ts
+++ backend/graphql/index.ts
@@ -23,9 +23,9 @@ const builder = new SchemaBuilder<{
   },
 });
 
-builder.prismaObject('Article', {
+builder.prismaNode('Article', {
+  id: { field: 'id' },
   fields: (t) => ({
-    id: t.exposeID('id'),
     title: t.exposeString('title'),
     description: t.exposeString('description'),
   }),
