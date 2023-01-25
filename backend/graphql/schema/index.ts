ed694fc7da050715d7434f131e6cf5f249da65fa
mizdra <pp.mizdra@gmail.com>
2023-01-26 00:05:09 +0900

    リファクタリング

--- backend/graphql/schema/index.ts
+++ backend/graphql/schema/index.ts
@@ -1,8 +1,15 @@
 import './post';
 import './hello';
+import { writeFileSync } from 'node:fs';
+import { printSchema, lexicographicSortSchema } from 'graphql';
 import { builder } from '../builder';
 
 builder.queryType();
 // builder.mutationType();
 
 export const schema = builder.toSchema();
+
+export function writeSchemaFile() {
+  const schemaAsString = printSchema(lexicographicSortSchema(schema));
+  writeFileSync('./config/schema.graphql', schemaAsString);
+}
--- script/generate-graphql-schema.ts
+++ script/generate-graphql-schema.ts
@@ -1,8 +1,5 @@
 #!/usr/bin/env -S npx ts-node --files
 
-import { writeFileSync } from 'node:fs';
-import { printSchema, lexicographicSortSchema } from 'graphql';
-import { schema } from '../backend/graphql/schema';
+import { writeSchemaFile } from '../backend/graphql/schema';
 
-const schemaAsString = printSchema(lexicographicSortSchema(schema));
-writeFileSync('./config/schema.graphql', schemaAsString);
+writeSchemaFile();
